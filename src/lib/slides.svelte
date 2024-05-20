c5045361bbd1327165165b2b7c6bf6eb2540c454
Hussein Awaad <hphantomass@gmail.com>
2024-05-21 02:07:53 +0300

    rotateer

---
+++ src/lib/slides.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import Webcam from "$lib/webcam.svelte";
+  import screenfull from "screenfull";
+  import { onMount } from "svelte";
+  let loaded = false;
+  let webcamer: Webcam;
+  let showbtn = true;
+
+  onMount(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.ready.then(() => {
+        loaded = true;
+      });
+    }
+  });
+</script>
+
+{#if loaded}
+  <div class="absolute top-[5vh] right-[2vw] flex flex-col gap-1">
+    <div class="flex gap-4">
+      <button><img class="max-w-2" src="/left.png" alt="" /></button>
+      <button><img class="max-w-2" src="/right.png" alt="" /></button>
+    </div>
+    <button on:click={webcamer.StartAnimations}
+      ><img class="max-w-4" src="/reset.png" alt="" /></button
+    >
+  </div>
+  <div
+    class:hidden={!showbtn}
+    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+  >
+    <button
+      class="bg-red-600 text-white p-4 font-bold text-xl"
+      on:click={() => {
+        showbtn = false;
+        if (screenfull.isEnabled) {
+          screenfull.request(undefined, { navigationUI: "hide" });
+        }
+      }}>FullScreen</button
+    >
+  </div>
+  <Webcam bind:this={webcamer} />
+{:else}
+  <p class="mx-auto my-auto text-3xl">Loading....</p>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,26 @@
 <script lang="ts">
-  import Webcam from "$lib/webcam.svelte";
-  import screenfull from "screenfull";
-
+  import WebcamSlider from "$lib/slides.svelte";
   import { onMount } from "svelte";
-  import { dev } from "$app/environment";
-  let showbtn = true;
-  let webcamer: Webcam;
-  let loaded = false;
+
+  let isPortrait = true;
   onMount(() => {
-    if ("serviceWorker" in navigator) {
-      // navigator.serviceWorker.register("/service-worker.js", {
-      //   type: dev ? "module" : "classic",
-      // });
-      // Wait until the service worker is ready
-      navigator.serviceWorker.ready.then(() => {
-        loaded = true;
-        console.log("Loaded All Data");
-      });
-    }
+    isPortrait = window.screen.orientation.type.includes("portrait");
+    window.screen.orientation.onchange = (e) => {
+      isPortrait = window.screen.orientation.type.includes("portrait");
+      // console.log({ data: window.screen.orientation, isPortrait });
+    };
   });
 </script>
 
 <div class="h-full w-full flex">
-  {#if loaded}
-    <div class="absolute top-[5vh] right-[2vw] flex flex-col gap-1">
-      <div class="flex gap-4">
-        <button><img class="max-w-2" src="/left.png" alt="" /></button>
-        <button><img class="max-w-2" src="/right.png" alt="" /></button>
-      </div>
-      <button on:click={webcamer.StartAnimations}
-        ><img class="max-w-4" src="/reset.png" alt="" /></button
-      >
-    </div>
-    <div
-      class:hidden={!showbtn}
-      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    >
-      <button
-        class="bg-red-600 text-white p-4 font-bold text-xl"
-        on:click={() => {
-          showbtn = false;
-          if (screenfull.isEnabled) {
-            screenfull.request(undefined, { navigationUI: "hide" });
-          }
-        }}>FullScreen</button
-      >
-    </div>
-    <Webcam bind:this={webcamer} />
-  {:else}
-    <p class="mx-auto my-auto text-3xl">Loading....</p>
-  {/if}
+  <div
+    class:hidden={!isPortrait}
+    class="h-full w-full flex items-center justify-center"
+  >
+    <img class="w-[25vw]" src="/rotatescreen.gif" alt="" />
+  </div>
+
+  <div class:hidden={isPortrait} class="h-full w-full">
+    <WebcamSlider />
+  </div>
 </div>
