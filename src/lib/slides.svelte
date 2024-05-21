32dc976b8305ef183c59f70df814e00122417f9d
Hussein Awaad <hphantomass@gmail.com>
2024-05-21 16:06:44 +0300

    element.msRequestFullscreen();

--- src/lib/slides.svelte
+++ src/lib/slides.svelte
@@ -15,18 +15,10 @@
   });
 
   function requestFullscreen(element: HTMLElement) {
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-      // Firefox
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-      // Chrome, Safari, Opera
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-      // IE/Edge
-      element.msRequestFullscreen();
-    }
+    element.requestFullscreen();
+    element.webkitRequestFullscreen();
+    element.mozRequestFullScreen();
+    element.msRequestFullscreen();
   }
 </script>
 
