2a20096168463025bba70e63d303ec26a85dbd9a
Hussein Awaad <hphantomass@gmail.com>
2024-05-22 16:17:56 +0300

    sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -19,6 +19,9 @@ for (let index = 0; index < counts.length; index++) {
   }
 }
 
+ASSETS.push(`${url}/AR/0.png`);
+ASSETS.push(`${url}/EN/1.png`);
+
 self.addEventListener("install", (event) => {
   // Create a new cache and add all files to it
   async function addFilesToCache() {
